4139fc67cbd6bb1acfc0295260fb7a70a9a1cad2
wangliuyong <1355498705@qq.com>
2018-07-21 12:55:16 +0800

    修改文件

---
+++ js/main.js
@@ -0,0 +1,9 @@
+PortfolioAll.onclick=function(){
+    PortfolioBar.className ="bar state-1";
+}
+PortfolioNative.onclick = function () {
+    PortfolioBar.className = "bar state-2";
+}
+PortfolioFramework.onclick = function () {
+    PortfolioBar.className = "bar state-3";
+}
