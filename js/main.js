9022714fbf7975d6112c1063ed3c1c7504e17dd8
wangliuyong <1355498705@qq.com>
2018-07-30 20:23:46 +0800

    添加二级菜单 添加鼠标·动画

--- js/main.js
+++ js/main.js
@@ -1,8 +1,78 @@
+//滚动时给导航栏添加渐变的背景
+window.onscroll=function(){
+    //console.log(window.scrollY);
+    var navBar = document.getElementById('navBar');
+    if (window.scrollY>0){
+        navBar.classList.add('sticky');
+    }else{
+        navBar.classList.remove("sticky");
+    }
+}
+//给子菜单添加鼠标进入事件
+//let li = document.getElementsByClassName('tiggerSubMenu');
+let li=document.querySelectorAll("nav>ul>li");
+console.log(li);
+for (let i = 0; i < li.length;i++){
+    li[i].onmouseenter=function(event){
+        /*
+        console.log(this);
+        let brother = this.getElementsByTagName("ul")[0];
+        let brother = event.currentTarget.nextSibling;
+        if (brother.nodeType === 3) {
+            brother = brother.nextSibling;
+        }
+        brother.classList.add("active");
+        */
+       this.classList.add("active");
+    }
+    li[i].onmouseleave = function (event) {
+        /*
+        let brother = this.getElementsByTagName("ul")[0];
+
+        let brother = event.currentTarget.nextSibling;
+        if (brother.nodeType === 3) {
+            brother = brother.nextSibling;
+        }
+        brother.classList.remove("active");
+        */
+       this.classList.remove("active");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//项目标题添加下划线
 PortfolioAll.onclick=function(){
     PortfolioBar.className ="bar state-1";
 }
 PortfolioNative.onclick = function () {
-    PortfolioBar.className = "bar state-2";
+    PortfolioBar.className = "bar state-2";``
 }
 PortfolioFramework.onclick = function () {
     PortfolioBar.className = "bar state-3";
