ba5d7f89767df73bf8cacf38bf29914e897ac68d
wangliuyong <1355498705@qq.com>
2018-08-16 17:34:46 +0800

    成功构建MVC模型

--- js/message.js
+++ js/message.js
@@ -1,7 +1,22 @@
 !function () {
     var model = {
-        fetch: () => { },
-        save: () => { }
+        //获取数据
+        fetch: function () {
+            //从云获取历史留言
+            var query = new AV.Query("message");
+            //注意这里，要用箭头函数。this内外指向一至。要不然里面就不饿能够用this，this指向花发生变化。
+            return query.find();
+        },
+        //存储数据
+        save: function (name,content) {
+            //创建message表
+            var xxx = AV.Object.extend("message");
+            //在message表中创建一行数据
+            var testObject = new xxx();
+            //数据的内容是 'content': content 保存
+            //如果保存成功执行consol
+            return testObject.save({ name: name, content: content })
+        }
     }
 
     var view = document.querySelector('section#LeaveMessage')
@@ -17,32 +32,27 @@
             this.loadMessage();
             this.bindEvent();
         },
-        initAV: () => {
+        initAV: function() {
             //初始化leancloud
             var APP_ID = "uIYIoLNwz3v4F1P0wIMobPvU-gzGzoHsz";
             var APP_KEY = "Kcm7BYxMpwGKtWHXBJW2ymVs";
             AV.init({ appId: APP_ID, appKey: APP_KEY });
         },
         loadMessage: function () {
-            //从云获取历史留言
-            var query = new AV.Query("message");
-            //注意这里，要用箭头函数。this内外指向一至。要不然里面就不饿能够用this，this指向花发生变化。
-            query
-                .find()
-                .then(messages => {
-                    let arr = messages.map(items => {
-                        return items.attributes;
-                    });
-                    arr.forEach(items => {
-                        let name = items.name;
-                        let content = items.content;
-                        let li = document.createElement("li");
-                        li.innerText = `${name}:${content}`;
-                        this.messageBoard.appendChild(li);
-                    });
-                    return AV.Object.saveAll(messages);
-                })
-                .then(function (messages) {
+
+            model.fetch().then((messages) => {
+                let arr = messages.map(items => {
+                    return items.attributes;
+                });
+                arr.forEach(items => {
+                    let name = items.name;
+                    let content = items.content;
+                    let li = document.createElement("li");
+                    li.innerText = `${name}:${content}`;
+                    this.messageBoard.appendChild(li);
+                });
+                return AV.Object.saveAll(messages);
+            }).then(function (messages) {
                     // 更新成功
                     console.log(messages);
                 }, function (error) {
@@ -51,38 +61,36 @@
                 });
         },
         bindEvent: function () {
-            //存储留言到云
+            
             this.myForm.addEventListener("submit", (event) => {
                 event.preventDefault();
-                
+                //存储留言到云
                 this.saveMessage();
             });
         },
         saveMessage: function () {
-            let myForm = this.myForm;
+            let myForm = controller.myForm;
             var content = myForm.querySelector('input[name="content"]').value;
             var name = myForm.querySelector('input[name="name"]').value;
-            let li = document.createElement("li");
-            li.innerText = `${name}:${content}`;
-            this.messageBoard.appendChild(li);
-            //创建message表
-            var xxx = AV.Object.extend("message");
-            //在message表中创建一行数据
-            var testObject = new xxx();
-            //数据的内容是 'content': content 保存
-            //如果保存成功执行consol
-            testObject
-                .save({ name: name, content: content })
-                .then(
+            if(content===''){
+                alert('请输入内容')
+            } else {
+                let li = document.createElement("li");
+                li.innerText = `${name}:${content}`;
+                controller.messageBoard.appendChild(li);
+
+                model.save(name, content).then(
                     function (object) {
                         console.log("存入一条数据");
                         console.log(content);
                     },
                     () => { }
                 );
+            }
+            
+
         }
     };
-
     controller.init();
 }.call()
 
