0a7bac1dfcbdf739dbcd6eae168d9481effbd41b
wangliuyong <1355498705@qq.com>
2018-08-16 17:00:17 +0800

    完成VC

--- js/message.js
+++ js/message.js
@@ -1,80 +1,87 @@
-!function(){
-    var model={
-        fetch:()=>{},
-        save:()=>{}
+!function () {
+    var model = {
+        fetch: () => { },
+        save: () => { }
     }
-    
-    var view=document.querySelector('section#LeaveMessage')
-    
-    
-    var controller={
-        view:null,
-        messageBoard:null,
-        myForm:null,
 
-        init:function(){
-            this.view=view;
-            this.messageBoard = document.querySelector('#historyMessage')
-            this.myForm = this.view.querySelector('#messageForm');
-            console.log(this.myForm)
+    var view = document.querySelector('section#LeaveMessage')
+
+
+    var controller = {
+        view: null, messageBoard: null, myForm: null,
+        init: function () {
+            this.view = view;
+            this.messageBoard = document.querySelector("#historyMessage");
+            this.myForm = this.view.querySelector("#messageForm");
             this.initAV();
             this.loadMessage();
             this.bindEvent();
         },
-
-        initAV:()=>{
+        initAV: () => {
             //初始化leancloud
-            var APP_ID = 'uIYIoLNwz3v4F1P0wIMobPvU-gzGzoHsz';
-            var APP_KEY = 'Kcm7BYxMpwGKtWHXBJW2ymVs';
+            var APP_ID = "uIYIoLNwz3v4F1P0wIMobPvU-gzGzoHsz";
+            var APP_KEY = "Kcm7BYxMpwGKtWHXBJW2ymVs";
             AV.init({ appId: APP_ID, appKey: APP_KEY });
         },
-
         loadMessage: function () {
             //从云获取历史留言
-            var query = new AV.Query('message');
+            var query = new AV.Query("message");
             //注意这里，要用箭头函数。this内外指向一至。要不然里面就不饿能够用this，this指向花发生变化。
-            query.find().then((messages) =>{
-                let arr = messages.map((items) => { return items.attributes })
-                arr.forEach((items) => {
-                    let name = items.name
-                    let content = items.content
-                    let li = document.createElement('li')
-                    li.innerText = `${name}:${content}`
-                    this.messageBoard.appendChild(li)
+            query
+                .find()
+                .then(messages => {
+                    let arr = messages.map(items => {
+                        return items.attributes;
+                    });
+                    arr.forEach(items => {
+                        let name = items.name;
+                        let content = items.content;
+                        let li = document.createElement("li");
+                        li.innerText = `${name}:${content}`;
+                        this.messageBoard.appendChild(li);
+                    });
+                    return AV.Object.saveAll(messages);
+                })
+                .then(function (messages) {
+                    // 更新成功
+                    console.log(messages);
+                }, function (error) {
+                    // 异常处理
+                    alert(error);
                 });
-                return AV.Object.saveAll(messages);
-            }).then(function (messages) {
-                // 更新成功
-                console.log(messages)
-            }, function (error) {
-                // 异常处理
-                alert(error)
+        },
+        bindEvent: function () {
+            //存储留言到云
+            this.myForm.addEventListener("submit", (event) => {
+                event.preventDefault();
+                
+                this.saveMessage();
             });
         },
-
-        bindEvent: function() {
-            //存储留言到云
-            this.myForm.addEventListener('submit', (event)=> {
-                event.preventDefault();
-                var content = this.myForm.querySelector('input[name="content"]').value;
-                var name = this.myForm.querySelector('input[name="name"]').value;
-                //创建message表
-                var xxx = AV.Object.extend('message');
-                //在message表中创建一行数据
-                var testObject = new xxx();
-                //数据的内容是 'content': content 保存
-                //如果保存成功执行consol
-                testObject.save({
-                    'name': name,
-                    'content': content
-                }).then(function (object) {
-                    window.location.reload()
-                    console.log('存入一条数据')
-                    console.log(content);
-                }, () => { })
-            })
+        saveMessage: function () {
+            let myForm = this.myForm;
+            var content = myForm.querySelector('input[name="content"]').value;
+            var name = myForm.querySelector('input[name="name"]').value;
+            let li = document.createElement("li");
+            li.innerText = `${name}:${content}`;
+            this.messageBoard.appendChild(li);
+            //创建message表
+            var xxx = AV.Object.extend("message");
+            //在message表中创建一行数据
+            var testObject = new xxx();
+            //数据的内容是 'content': content 保存
+            //如果保存成功执行consol
+            testObject
+                .save({ name: name, content: content })
+                .then(
+                    function (object) {
+                        console.log("存入一条数据");
+                        console.log(content);
+                    },
+                    () => { }
+                );
         }
-    }
+    };
 
     controller.init();
 }.call()
